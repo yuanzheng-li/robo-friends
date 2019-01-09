e835975c1cd4035b24ed781d96db7ec82d2b9fb8
Yuanzheng Li <yuanzhengliwork@gmail.com>
2019-01-08 23:35:11 -0800

    Updates

---
+++ precache-manifest.b5eaccfcac5f5c413d293a66abc4ef4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "abe515def0d9bc7dd98c",
+    "url": "/robo-friends/static/js/main.abe515de.chunk.js"
+  },
+  {
+    "revision": "77fefb85fc6b471a30b1",
+    "url": "/robo-friends/static/js/1.77fefb85.chunk.js"
+  },
+  {
+    "revision": "abe515def0d9bc7dd98c",
+    "url": "/robo-friends/static/css/main.33549739.chunk.css"
+  },
+  {
+    "revision": "77fefb85fc6b471a30b1",
+    "url": "/robo-friends/static/css/1.ffcdec88.chunk.css"
+  },
+  {
+    "revision": "3718acb40833c2f9806acc42635a5f41",
+    "url": "/robo-friends/index.html"
+  }
+];
